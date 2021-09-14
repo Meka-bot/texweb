15ccaeadc80871d0ab98c12b26443eec18abe394
CARLOS RUIZ DE AZUA FUENTES <solracraf@outlook.com>
2021-09-13 20:52:54 -0500

    wo wo

--- resources/views/archivo.blade.php
+++ resources/views/archivo.blade.php
@@ -4,11 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Proyecto Blog</title>
+    <title>Nombre</title>
 </head>
 <body>
-    <h1>Howdy</h1>
-    
-
+    <h1>My name is Charlie Fuentes</h1>
 </body>
 </html>
